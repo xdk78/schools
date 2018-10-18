50d0b55b7023b93525a4191cffeea1595384f63d
xdk78 <xdk78888@gmail.com>
2018-10-18 20:23:35 +0200

    fix(vulcan): timetable reducer days

--- lib/store/timetable/timetable_reducer.dart
+++ lib/store/timetable/timetable_reducer.dart
@@ -1,7 +1,6 @@
 import 'package:redux/redux.dart';
 import 'package:schools/store/timetable/timetable_state.dart';
 import 'package:schools/store/timetable/timetable_actions.dart';
-import 'package:schools/api/vulcan/actions.dart';
 
 Reducer<TimetableState> timetableReducer = combineReducers<TimetableState>(
     [TypedReducer<TimetableState, SetTimetableAction>(_setTimetable)]);
@@ -9,8 +8,8 @@ Reducer<TimetableState> timetableReducer = combineReducers<TimetableState>(
 TimetableState _setTimetable(TimetableState state, SetTimetableAction action) {
   return state.rebuild((b) => b
     ..monday.replace(action.monday)
-    ..tuesday.replace(action.monday)
-    ..wednesday.replace(action.monday)
-    ..thursday.replace(action.monday)
-    ..friday.replace(action.monday));
+    ..tuesday.replace(action.tuesday)
+    ..wednesday.replace(action.wednesday)
+    ..thursday.replace(action.thursday)
+    ..friday.replace(action.friday));
 }
